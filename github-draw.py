bfb260d433e0549b99564014ef899489aaa655b2
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2018-12-22 15:13:25 +0530

    test

--- github-draw.py
+++ github-draw.py
@@ -19,8 +19,6 @@ d = datetime.datetime.today()
 last_Sunday = last_sunday(d) 
 gitlog_start = last_Sunday - datetime.timedelta(364) #find the starting point of github yearly activity
 
-
-
 p = cv2.imread(sys.argv[1],0)
 #cv2.imshow('image',p)
 p=p.transpose()
@@ -30,10 +28,12 @@ command = "git commit -a -m \"Commit-{d}\" --date={d}"
 with open("temp.txt", "a") as myfile:
     for x in range(0, 52):
         for y in range(0,7):
-            if p[x][y]!=255:
-                for n in range(0, math.floor((255-p[x][y])/10)): #commit according to color
-                    #print(n)
-                    myfile.write("1") #need changes for github commit
-                    myfile.flush()
-                    call(command.format(d=str(gitlog_start+datetime.timedelta(x*7+y))), shell=True) #commit command
-                    i=i+1
+            if not p[x][y]!=255:
+                continue
+            for n in range(0, math.floor((255-p[x][y])/10)): #commit according to color
+                #print(n)
+                myfile.write("1") #need changes for github commit
+                myfile.flush()
+                import pdb; pdb.set_trace()
+                call(command.format(d=str(gitlog_start+datetime.timedelta(x*7+y))), shell=True) #commit command
+                i=i+1
